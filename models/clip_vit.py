09d1945c191ca20cc3c6ebe25916dabb2f4f97d0
sueszli <jabaryyahya@gmail.com>
2024-07-27 23:03:08 +0200

    up

---
+++ models/clip_vit.py
@@ -0,0 +1,5 @@
+# https://github.com/openai/CLIP
+# https://github.com/wang-research-lab/roz/blob/main/scripts/natural_distribution_shift/src/models/CLIPViT.py
+
+# import torchvision.models as models
+# model = models.vit_b_16(pretrained=True)
---
+++ models/registry.py
@@ -0,0 +1,1 @@
+# https://github.com/wang-research-lab/roz/blob/main/scripts/natural_distribution_shift/src/registry.py
