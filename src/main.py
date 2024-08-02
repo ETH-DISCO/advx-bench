8fa2c25a0783ad68b355ab928c24dd5ddfbc8cb7
sueszli <jabaryyahya@gmail.com>
2024-08-02 19:34:31 +0200

    up

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from models.registry import get_model
+
+if __name__ == "__main__":
+    # model = get_model("ExampleModel", x=1, y=2)
+    # print(model)
+    # print(model.x, model.y)
+    print("yo")
