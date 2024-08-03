de44be77b0be9e416f8f866f976fa70397b08e7a
sueszli <jabaryyahya@gmail.com>
2024-08-03 10:28:42 +0200

    up

--- src/cls.py
+++ src/cls.py
@@ -8,7 +8,7 @@ def get_clip_predictions(img: Image.Image, labels: list[str]) -> dict[str, float
     import clip
 
     device = get_device()
-    model, preprocess = clip.load("ViT-L/14@336px", device=device)  # largest ViT
+    model, preprocess = clip.load("ViT-L/14@336px", device=device)
     model.eval()
 
     text = clip.tokenize(labels).to(device)
@@ -28,17 +28,17 @@ def get_open_coca_predictions(img: Image.Image, labels: list[str]) -> dict[str, 
     model.eval()
     tokenizer = open_clip.get_tokenizer('coca_ViT-L-14')
 
-    image = preprocess(image).unsqueeze(0)
+    image = preprocess(img).unsqueeze(0)
     text = tokenizer(labels)
 
-    with torch.no_grad(), torch.cuda.amp.autocast():
+    with torch.no_grad():
         image_features = model.encode_image(image)
         text_features = model.encode_text(text)
         image_features /= image_features.norm(dim=-1, keepdim=True)
         text_features /= text_features.norm(dim=-1, keepdim=True)
-        probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+        text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
 
-    return {label: prob for label, prob in zip(labels, probs[0])}
+    return {label: prob.item() for label, prob in zip(labels, text_probs[0])}
 
 
 # TODO: eva
