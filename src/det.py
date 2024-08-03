02d5d44ed39a90fcefb937f128c2ffd1dcca9aaa
sueszli <jabaryyahya@gmail.com>
2024-08-03 12:01:26 +0200

    up

--- src/det.py
+++ src/det.py
@@ -46,9 +46,42 @@ def get_vit_predictions(img: Image.Image, labels: list[str], threshold: float) -
 
     results[0]["boxes"] = [elem.tolist() for elem in results[0]["boxes"]]
     results[0]["scores"] = [elem.item() for elem in results[0]["scores"]]
-    results[0]["labels"] = [labels[elem] for elem in results[0]["labels"]]
+    results[0]["labels"] = [labels[elem.item()] for elem in results[0]["labels"]]
+
     boxes, scores, labels = results[0]["boxes"], results[0]["scores"], results[0]["labels"]
     return boxes, scores, labels
+
+
+def get_detr_detections(img: Image.Image, threshold: float) -> tuple[list[list[float]], list[float], list[str]]:
+    from transformers import AutoImageProcessor, DetrForObjectDetection
+
+    model_id = "facebook/detr-resnet-101-dc5"
+    image_processor = AutoImageProcessor.from_pretrained(model_id)
+    model = DetrForObjectDetection.from_pretrained(model_id)
+
+    # no labels
+    inputs = image_processor(images=img, return_tensors="pt")
+    outputs = model(**inputs)
+
+    target_sizes = torch.tensor([img.size[::-1]])
+    results = image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[0]
+    results["boxes"] = [elem.tolist() for elem in results["boxes"]]
+    results["scores"] = [elem.item() for elem in results["scores"]]
+    results["labels"] = [model.config.id2label[elem.item()] for elem in results["labels"]]
+
+    # alternatively: map model labels to custom labels
+    # model_labels = [model.config.id2label[elem.item()] for elem in results["labels"]]
+    # results["labels"] = []
+    # for ml in model_labels:
+    #     for cl in labels:
+    #         if cl.lower() in ml.lower():
+    #             results["labels"].append(cl)
+    #             break
+    #     else:
+    #         results["labels"].append("unknown")
+
+    results = results["boxes"], results["scores"], results["labels"]
+    return results
 
 
 def plot_results(results: tuple[list[list[float]], list[float], list[str]]):
@@ -67,7 +100,7 @@ threshold = 0.1
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 img = Image.open(requests.get(url, stream=True).raw)
 
-
 # results = get_groundingdino_predictions(img, labels, threshold)
-results = get_vit_predictions(img, labels, threshold)
+# results = get_vit_predictions(img, labels, threshold)
+results = get_detr_detections(img, threshold)
 plot_results(results)
