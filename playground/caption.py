52155e13ae6b4187ef2bca7a8879217862b95bdf
sueszli <jabaryyahya@gmail.com>
2024-08-06 15:05:06 +0200

    up

--- playground/caption.py
+++ playground/caption.py
@@ -8,15 +8,13 @@ model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-capt
 img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
 raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
 
-# conditional image captioning
-text = "a photography of"
-inputs = processor(raw_image, text, return_tensors="pt")
+# v1: confitional img captioning
+# text = "a photography of"
+# inputs = processor(raw_image, text, return_tensors="pt")
+# out = model.generate(**inputs)
+# print(processor.decode(out[0], skip_special_tokens=True))
 
-out = model.generate(**inputs)
-print(processor.decode(out[0], skip_special_tokens=True))
-
-# unconditional image captioning
-inputs = processor(raw_image, return_tensors="pt")
-
+# v2: unconditional image captioning
+inputs = processor(raw_image, return_tensors="pt") 
 out = model.generate(**inputs)
 print(processor.decode(out[0], skip_special_tokens=True))
