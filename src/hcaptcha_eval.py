7d95ef6d953952809e8de65272b733dd5c210e26
sueszli <jabaryyahya@gmail.com>
2024-08-12 16:10:43 +0200

    up

--- src/hcaptcha_eval.py
+++ src/hcaptcha_eval.py
@@ -4,16 +4,18 @@ from pathlib import Path
 import numpy as np
 import torch
 from PIL import Image
+from safetensors.torch import save_file
 from tqdm import tqdm
 
-from safetensors.torch import save_file
 from models.caption import caption_blip
 from models.cls import classify_metaclip
 from models.det import detect_vit
 from models.seg import segment_sam1
 
+
 def ensure_contiguous(tensor):
     return tensor.contiguous() if not tensor.is_contiguous() else tensor
+
 
 datapath = Path.cwd() / "data" / "hcaptcha"
 outputpath = Path.cwd() / "data" / "hcaptcha-eval"
@@ -25,6 +27,10 @@ for subdir in subdirs:
 
     for file in tqdm(list(subdir.glob("*.png"))):
         filename = file.stem
+
+        if (outputpath / f"{categoryname}_{filename}.safetensors").exists():
+            print(f"file already exists: {categoryname}_{filename}.safetensors")
+            continue
 
         img = Image.open(file)
         threshold = 0.1
@@ -42,13 +48,14 @@ for subdir in subdirs:
         img_tensor = ensure_contiguous(torch.from_numpy(np.array(img)).permute(2, 0, 1).float() / 255.0)
         data_dict = {
             "image": img_tensor,
-            "captions": ensure_contiguous(torch.tensor([ord(c) for c in '|'.join(captions)], dtype=torch.int32)),
+            "captions": ensure_contiguous(torch.tensor([ord(c) for c in "|".join(captions)], dtype=torch.int32)),
             "classification_probs": ensure_contiguous(torch.tensor(probs, dtype=torch.float32)),
             "detection_boxes": ensure_contiguous(torch.tensor(boxes, dtype=torch.float32)),
             "detection_scores": ensure_contiguous(torch.tensor(scores, dtype=torch.float32)),
-            "detection_labels": ensure_contiguous(torch.tensor([ord(c) for c in '|'.join(labels)], dtype=torch.int32)),
-            "segmentation_masks": ensure_contiguous(masks)
+            "detection_labels": ensure_contiguous(torch.tensor([ord(c) for c in "|".join(labels)], dtype=torch.int32)),
+            "segmentation_masks": ensure_contiguous(masks),
         }
-        
+
         save_file(data_dict, outputpath / f"{categoryname}_{filename}.safetensors")
         print(f"saved {categoryname}_{filename}.safetensors")
+        os.system(f"git add . && git commit -m 'autocommit' && git push")
