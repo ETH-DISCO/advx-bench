d886c90549b87f4621922ad236bca81615494438
sueszli <jabaryyahya@gmail.com>
2024-08-12 11:27:17 +0200

    up

--- src/models/generation.py
+++ src/models/generation.py
@@ -10,9 +10,24 @@ try:
 except ImportError:
     from utils import get_device
 
+
 """
 models
 """
+
+
+def gen_fluxv1(prompt: str) -> Image.Image:
+    assert torch.cuda.is_available(), "GPU not available"
+    import torch
+    from diffusers import FluxPipeline
+
+    pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16).to("cuda")
+    # pipe.enable_model_cpu_offload() # uncomment if you have a weak GPU
+
+    images = pipe(prompt, height=1024, width=1024, guidance_scale=3.5, num_inference_steps=50, max_sequence_length=512, generator=torch.Generator("cpu").manual_seed(0))
+    img = images.images[0]
+    assert isinstance(img, Image.Image)
+    return img
 
 
 def gen_stable_diffusion(prompt: str) -> Image.Image:
