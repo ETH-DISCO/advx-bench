8643fe79bde287850bf7adc6af34b96eb843b58f
sueszli <jabaryyahya@gmail.com>
2024-08-15 23:49:49 +0200

    up

--- data/custom/circles.py
+++ data/custom/circles.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import cairo
 
-WIDTH, HEIGHT = 600, 600
+WIDTH, HEIGHT = 1000, 1000
 
 surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 context = cairo.Context(surface)
@@ -15,15 +15,19 @@ def draw_concentric_circles(x, y, max_radius, num_rings):
         radius = max_radius * (i + 1) / num_rings
         context.arc(x, y, radius, 0, 2 * math.pi)
 
-        # blue -> yellow -> red
         color_ratio = i / (num_rings - 1)
-        if color_ratio < 0.5:
-            # blue -> yellow
-            rgb_color = (color_ratio * 2, color_ratio * 2, 1 - color_ratio * 2)
+        if color_ratio < 0.10:
+            # red
+            rgb_color = (1, 0, 0)
+        elif color_ratio < 0.5:
+            # gray
+            rgb_color = (1, 1, 0)
+        elif color_ratio < 0.75:
+            # yellow
+            rgb_color = (0.5, 0.5, 0.5)
         else:
-            # yellow -> red
-            rgb_color = (1, 2 - color_ratio * 2, 0)
-
+            # blue
+            rgb_color = (0, 0, 1)
 
         context.set_source_rgb(*rgb_color)
         context.set_line_width(1.5)
@@ -31,7 +35,7 @@ def draw_concentric_circles(x, y, max_radius, num_rings):
 
 
 count = 3
-num_rings = 15
+num_rings = 12
 max_radius = WIDTH / 2 / count
 
 for row in range(count):
@@ -39,6 +43,5 @@ for row in range(count):
         x = (col + 0.5) * WIDTH / count
         y = (row + 0.5) * WIDTH / count
         draw_concentric_circles(x, y, max_radius, num_rings)
-
 
 surface.write_to_png(Path("data/custom/circles.png"))
---
+++ data/custom/text.py
@@ -0,0 +1,46 @@
+import random
+from pathlib import Path
+import math
+import cairo
+
+WIDTH, HEIGHT = 1000, 1000
+
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
+context = cairo.Context(surface)
+context.set_source_rgba(0, 0, 0, 0)
+context.paint()
+
+
+words = ["banana", "apple", "orange", "grape", "pear", "donkey", "elephant", "giraffe", "hippopotamus", "kangaroo"]
+
+num_words = 15
+font_range = (20, 100)
+
+for i in range(num_words):
+    
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
+    context.set_font_size(random.randint(*font_range))
+
+
+    word = random.choice(words)
+    orientation = random.choice(["horizontal", "vertical", "flipped"])
+
+    context.save()
+    context.translate(x, y)
+
+    if orientation == "vertical":
+        context.rotate(-math.pi/2)
+    elif orientation == "flipped":
+        context.rotate(random.uniform(0, 2*math.pi))
+        context.scale(-1 if random.random() > 0.5 else 1, -1 if random.random() > 0.5 else 1)
+
+    grayshade = random.random()
+    context.set_source_rgb(grayshade, grayshade, grayshade)
+    
+    context.move_to(0, 0)
+    context.show_text(word)
+    
+    context.restore()
+
+surface.write_to_png(Path("data/custom/text.png"))
