48ece96355d8aef2d14f4a9f0178aaf4db14d417
sueszli <jabaryyahya@gmail.com>
2024-08-16 02:00:39 +0200

    autocommit

--- src/openai.sh
+++ src/openai.sh
@@ -1,2 +1,21 @@
-# read the first argument passed to the script and echo
-echo $1
+#!/bin/bash
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -k|--key) key="$2"; shift ;;
+        *) echo "unknown parameter passed, use --key flag: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+echo -e "key=$key"
+
+curl https://api.openai.com/v1/images/generations \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $key" \
+  -d '{
+    "model": "dall-e-3",
+    "prompt": "an image of a photorealistic city in a desert where all colors are inverted and a blue camel in the right with very small hoofs. psychedelic surrealism.",   
+    "n": 1,           
+    "size": "1024x1024"
+  }'
