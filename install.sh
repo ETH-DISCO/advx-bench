2da1aad051a11648261c40ed2fc29cac9c328be1
sueszli <jabaryyahya@gmail.com>
2024-08-21 23:31:00 +0200

    up

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
+python3 -m venv .venv && source .venv/bin/activate
+
+pip install -r requirements.txt
+
 pip install git+https://github.com/openai/CLIP.git
 pip install open-clip-torch
 python -m spacy download en_core_web_sm
 pip install clip diffusers matplotlib numpy opencv_python opencv_python_headless Pillow Requests spacy torch transformers accelerate
 
-# openai
+# enter openai api key
 export OPENAI_API_KEY="sk-xxxxxx"
 
 # run
-python3 -m venv .venv
-source .venv/bin/activate
 nohup $PWD/.venv/bin/python3 $PWD/src/XYZ.py > output.log 2>&1 &
 nohup $PWD/.venv/bin/python3 $PWD/src/3-eval_cls_perturb.py > output.log 2>&1 &
 
