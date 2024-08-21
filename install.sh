37e5ae72fe619dc1375db2d86431996ddc3459fb
sueszli <jabaryyahya@gmail.com>
2024-08-21 12:01:50 +0200

    up

--- install.sh
+++ install.sh
@@ -2,3 +2,13 @@ pip install git+https://github.com/openai/CLIP.git
 pip install open-clip-torch
 python -m spacy download en_core_web_sm
 pip install clip diffusers matplotlib numpy opencv_python opencv_python_headless Pillow Requests spacy torch transformers accelerate
+
+# run nohup
+python3 -m venv .venv
+source .venv/bin/activate
+nohup $PWD/.venv/bin/python3 $PWD/src/XYZ.py > output.log 2>&1 &
+nohup $PWD/.venv/bin/python3 $PWD/src/2-eval_cls_mask.py > output.log 2>&1 &
+
+# check process
+ps aux | grep advx-bench
+htop
--- run.sh
+++
@@ -1,7 +0,0 @@
-# run nohup
-source .venv/bin/activate
-nohup $PWD/.venv/bin/python3 $PWD/src/2-eval_cls_mask.py > output.log 2>&1 &
-
-# check process
-ps aux | grep advx-bench
-htop
