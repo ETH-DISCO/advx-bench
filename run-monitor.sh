161ce2e8e7965925eea094df4a6d5d3df0f995e4
sueszli <jabaryyahya@gmail.com>
2024-08-25 23:08:02 +0200

    up

--- run-monitor.sh
+++ run-monitor.sh
@@ -11,6 +11,10 @@ $PWD/.venv/bin/python3 "$python_file" > run.log 2>&1 & echo $! > run.pid
 while true; do
     if [ -f "run.pid" ] && ! ps -p $(cat "run.pid") > /dev/null 2>&1; then
         echo "process died, restarting..."
+
+        rm -f run.pid
+        rm -f run.log
+
         $PWD/.venv/bin/python3 "$python_file" > run.log 2>&1 & echo $! > run.pid
     fi
     sleep 5 # check every 5 seconds
