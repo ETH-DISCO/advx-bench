a2152dcf7cbd4f7a8ddaf93d11338385a34cf2dc
sueszli <jabaryyahya@gmail.com>
2024-08-25 22:40:56 +0200

    up

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+export OPENAI_API_KEY="sk-xxxxxx"
+
+
 python3 -m venv .venv && source .venv/bin/activate
 
 pip install -r requirements.txt
@@ -8,9 +11,20 @@ python -m spacy download en_core_web_sm
 pip install clip diffusers matplotlib numpy opencv_python opencv_python_headless Pillow Requests spacy torch transformers accelerate
 
 
-export OPENAI_API_KEY="sk-xxxxxx"
-
 python_file="./src/4-eval_cls_perturb.py"
+pid_file="pid.txt"
+
+# initial start
 nohup $PWD/.venv/bin/python3 "$python_file" > output.log 2>&1 & echo $! > "$pid_file"
+
+# endless loop to monitor and restart the process
+while true; do
+    if ! ps -p $(cat "$pid_file") > /dev/null; then
+        echo "Process died, restarting..."
+        nohup $PWD/.venv/bin/python3 "$python_file" > output.log 2>&1 & echo $! > "$pid_file"
+    fi
+    sleep 5 # check every 5 seconds
+done
+
 
 watch -n 0.1 "tail -n 100 output.log"
