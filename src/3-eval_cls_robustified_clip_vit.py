d8a50f267e1129e7929b5b6ae1fd676939a0306c
sueszli <jabaryyahya@gmail.com>
2024-08-27 15:33:11 +0200

    up

--- src/3-eval_cls_robustified_clip_vit.py
+++ src/3-eval_cls_robustified_clip_vit.py
@@ -1,20 +1,37 @@
+import csv
 import gc
 import json
 import os
 from pathlib import Path
 
 import torch
+import torchvision.transforms as transforms
 from datasets import load_dataset
 from PIL import Image
 
 from advx.masks import get_diamond_mask
 from advx.utils import add_overlay
+from metrics.metrics import get_cosine_similarity, get_psnr, get_ssim
 from models.cls import classify_clip, classify_robustified_clip
 from utils import get_device, set_seed
 
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+
+
+def is_cached(path: Path, entry_id: dict) -> bool:
+    entry_id = entry_id.copy()
+
+    if not path.exists():
+        return False
+
+    with open(path, mode="r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if all(row[key] == str(value) for key, value in entry_id.items()):
+                return True
+    return False
 
 
 def get_imagenet_label(idx: int) -> str:
@@ -54,7 +71,17 @@ if device == "cuda":
     torch.cuda.reset_accumulated_memory_stats()
 
 for id, img, label_id, caption in dataset:
+    entry_id = {
+        "img_id": id,
+    }
+    if is_cached(outpath, entry_id):
+        print(f"skipping {entry_id}")
+        continue
+
     adv_img = add_overlay(img.convert("RGB"), overlay=overlay, opacity=160).convert("RGB")
+    transform = transforms.Compose([transforms.Resize((256, 256)), transforms.Grayscale(num_output_channels=3), transforms.ToTensor()])
+    x: torch.Tensor = transform(img).unsqueeze(0)
+    advx_x: torch.Tensor = transform(adv_img).unsqueeze(0)
 
     def get_acc_boolmask(img: Image.Image, model: torch.nn.Module) -> list[bool]:
         preds = list(zip(range(len(labels)), model(img, labels)))
@@ -68,6 +95,26 @@ for id, img, label_id, caption in dataset:
 
     print(f"original: {original_top5_boolmask}")
     print(f"robustified: {robustified_top5_boolmask}")
+
+    results = {
+        **entry_id,
+        # semantic similarity
+        "cosine_sim": get_cosine_similarity(img, adv_img),
+        "psnr": get_psnr(x, advx_x),
+        "ssim": get_ssim(x, advx_x),
+        # accuracy
+        "label": get_imagenet_label(label_id),
+        "original_acc1": 1 if original_top5_boolmask[0] else 0,
+        "robustified_acc1": 1 if robustified_top5_boolmask[0] else 0,
+        "original_acc5": 1 if any(original_top5_boolmask) else 0,
+        "robustified_acc5": 1 if any(robustified_top5_boolmask) else 0,
+    }
+
+    with open(outpath, mode="a") as f:
+        writer = csv.DictWriter(f, fieldnames=results.keys())
+        if outpath.stat().st_size == 0:
+            writer.writeheader()
+        writer.writerow(results)
 
     torch.cuda.empty_cache()
     gc.collect()
