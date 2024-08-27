d171476dabd7fb4f063bf7302d79056e1afef98d
sueszli <jabaryyahya@gmail.com>
2024-08-27 13:13:57 +0200

    up

--- src/4-eval_cls_robustified_clip_vit.py
+++ src/4-eval_cls_robustified_clip_vit.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import torch
 from datasets import load_dataset
 
-
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
