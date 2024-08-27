c53733a6d3764147839baacbbf03932ba8ce1ee7
sueszli <jabaryyahya@gmail.com>
2024-08-27 14:58:32 +0200

    up

--- src/3-eval_cls_robustified_clip_vit.py
+++ src/3-eval_cls_robustified_clip_vit.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import torch
 from datasets import load_dataset
+from huggingface_hub import hf_hub_download
 
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
@@ -17,3 +18,7 @@ seed = 41
 
 outpath = Path.cwd() / "data" / "eval" / "eval_cls.csv"
 dataset = load_dataset("visual-layer/imagenet-1k-vl-enriched", split="validation", streaming=True).shuffle(seed=seed)
+
+file_name = "robustified_clip_vit.pth"
+model_id = "sueszli/robustified_clip_vit"
+downloaded_file_path = hf_hub_download(repo_id=model_id, filename=file_name)
