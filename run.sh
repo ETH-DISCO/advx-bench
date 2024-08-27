a80f8e6c2d13ab07aa75d97f1229d6258a215192
sueszli <jabaryyahya@gmail.com>
2024-08-27 18:46:28 +0200

    up

--- run.sh
+++ run.sh
@@ -7,3 +7,7 @@ pip install open-clip-torch
 python -m spacy download en_core_web_sm
 pip install clip diffusers matplotlib numpy opencv_python opencv_python_headless Pillow Requests spacy torch transformers accelerate
 
+
+make monitor filepath="./src/3-train_cls_robustified_clip_vit.py"
+
+# nohup .venv/bin/python3 src/XYZ.py >> "monitor-process.log" 2>&1 &
